b42df36b35a227316d4f4505bf25d2ad9824ff31
Kyle Dorsey <KylePDorsey@gmail.com>
2016-09-30 13:55:37 -0500

    added new commands, faker

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,10 +10,21 @@ post '/' do
   return 200 if username == "slackbot" # or whatever your bot is named
 
   if message.include? "duke"
-    # sleep(2) optional
+    if message.include? "joke"
+      return JSON.dump({
+        "text" => Faker::SlackEmoji.objects_and_symbols
+      })
+    else message.include? "hack"
+      return JSON.dump({
+        "text" => Faker::Hacker.say_something_smart
+      })
+    end
+  else
     return JSON.dump({
       "text" => "Did someone say my name?"
     })
   end
+
+
 
 end
